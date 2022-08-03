bde930dca5fb89212653d7f7e8c270d20242904c
XMuminov <xurshidbek4343@gmail.com>
2022-08-03 17:32:31 +0500

    change

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,12 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        "dark-green": "#009289"
+        "dark-green": "#009289",
+        "gray-black": "#5a5a5a",
       },
       maxWidth: {
         134: '134px',
+        1544: '1544px',
       }
     },
   },
